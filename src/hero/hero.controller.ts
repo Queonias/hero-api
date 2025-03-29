55b3627673548d815080fdad1d3903b5417f9887
queones <queones@gmail.com>
2025-03-29 18:24:01 -0300

    feat: finalização do CRUD

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { HeroDto } from './dto/hero.dto';
 import { HeroService } from './hero.service';
 
@@ -9,5 +17,25 @@ export class HeroController {
   @Post()
   async create(@Body() data: HeroDto) {
     return this.heroService.create(data);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.heroService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() data: HeroDto) {
+    return this.heroService.update(Number(id), data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.heroService.delete(Number(id));
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number) {
+    return this.heroService.getById(Number(id));
   }
 }
--- src/hero/hero.service.ts
+++ src/hero/hero.service.ts
@@ -5,11 +5,56 @@ import { PrismaService } from 'src/database/prisma.service';
 @Injectable()
 export class HeroService {
   constructor(private prisma: PrismaService) {}
+
   async create(data: HeroDto) {
     const hero = await this.prisma.hero.create({
       data,
     });
 
     return hero;
+  }
+  async findAll() {
+    return this.prisma.hero.findMany();
+  }
+
+  async update(id: number, data: HeroDto) {
+    const heroExists = await this.prisma.hero.findUnique({
+      where: { id },
+    });
+
+    if (!heroExists) {
+      throw new Error('Heroi não encontrado');
+    }
+
+    return await this.prisma.hero.update({
+      data,
+      where: { id },
+    });
+  }
+
+  async delete(id: number) {
+    const heroExists = await this.prisma.hero.findUnique({
+      where: { id },
+    });
+
+    if (!heroExists) {
+      throw new Error('Heroi não encontrado');
+    }
+
+    return await this.prisma.hero.delete({
+      where: { id },
+    });
+  }
+
+  async getById(id: number) {
+    const heroExists = await this.prisma.hero.findUnique({
+      where: { id },
+    });
+
+    if (!heroExists) {
+      throw new Error('Heroi não encontrado');
+    }
+
+    return heroExists;
   }
 }
