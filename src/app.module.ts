b9686ebff0c59f64d7b6da9bcd1e9806bdad38db
queones <queones@gmail.com>
2025-03-29 16:10:59 -0300

    feat: controller criado

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroService } from './hero/hero.service';
+import { HeroController } from './hero/hero.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HeroController],
   providers: [AppService, HeroService],
 })
 export class AppModule {}
